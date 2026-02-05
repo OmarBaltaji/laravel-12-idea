7f8cdff8b0edf4f4b9269d6bdc871f54f9e952a0
Omar Baltaji <omr.baltaji@gmail.com>
2026-02-05 22:19:49 +0200

    Setting up boot methods to ensure Models are unguarded, prevent lazy loading and forcing eager load by default

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Doesn't guard models
+        Model::unguard();
+        // Prevents lazy loading (n +1) and prevents accessing eloquent model attributes that don't exist
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
     }
 }
