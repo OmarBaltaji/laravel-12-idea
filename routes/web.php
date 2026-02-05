4296db8190baa82e86647ad926d3f6adea55d55c
Omar Baltaji <omr.baltaji@gmail.com>
2026-02-05 22:22:01 +0200

    Defining necessary routes for login and register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionsController;
 
 Route::get('/', fn () => view('welcome'));
+
+Route::middleware('guest')->group(function() {
+  Route::get('/register', [RegisteredUserController::class, 'create']);
+  Route::post('/register', [RegisteredUserController::class, 'store']);
+  
+  Route::get('/login', [SessionsController::class, 'create']);
+  Route::post('/login', [SessionsController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function() {
+  Route::post('/logout', [SessionsController::class, 'destroy']);
+});
