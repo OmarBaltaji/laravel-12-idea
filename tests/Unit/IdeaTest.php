886aced77d29f93ac784ad2f27a7040d7b4234be
Omar Baltaji <omr.baltaji@gmail.com>
2026-02-05 22:22:39 +0200

    Writing tests to ensure relations are defined and loaded correctly

---
+++ tests/Unit/IdeaTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Idea;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
+// To prove that Idea has a relation user()
+test('it belongs to a user', function() {
+    $idea = Idea::factory()->create();
+
+    expect($idea->user)->toBeInstanceOf(User::class);
+});
+
+test('it can have steps', function() {
+    $idea = Idea::factory()->create();
+
+    // expect($idea->steps)->toBeInstanceOf(Collection::class);
+    expect($idea->steps)->toBeEmpty();
+
+    $idea->steps()->create([
+        'description' => 'Do the thing',
+    ]);
+
+    expect($idea->fresh()->steps)->toHaveCount(1);
+});
