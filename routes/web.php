8924ee2c7a875bcd06ce2c18a02b9e2dab2b40f5
Omar Baltaji <omr.baltaji@gmail.com>
2026-02-07 20:18:03 +0200

    Adding ideas list and show route

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\IdeaController;
 
-Route::get('/', fn () => view('welcome'));
+// Route::get('/', fn () => view('welcome'));
+
+// Welcome page is where marketing is done to encourage users to signup
+Route::redirect('/', 'ideas');
 
 Route::middleware('guest')->group(function() {
   Route::get('/register', [RegisteredUserController::class, 'create']);
   Route::post('/register', [RegisteredUserController::class, 'store']);
   
-  Route::get('/login', [SessionsController::class, 'create']);
+  Route::get('/login', [SessionsController::class, 'create'])->name('login');
   Route::post('/login', [SessionsController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function() {
+  Route::get('/ideas', [IdeaController::class, 'index'])->name('ideas.index');
+  Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
   Route::post('/logout', [SessionsController::class, 'destroy']);
 });
