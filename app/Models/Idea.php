0fb7c7cadf0f1fe7001b9327bf190e4ae8acee1f
Omar Baltaji <omr.baltaji@gmail.com>
2026-02-08 13:13:37 +0200

    Creating count function to retrieve count per status

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -1,13 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use App\IdeaStatus;
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Idea extends Model
 {
@@ -22,6 +25,21 @@ class Idea extends Model
     protected $attributes = [
         'status' => IdeaStatus::PENDING->value,
     ];
+
+    public static function statusCount(User $user): Collection
+    {
+        $counts = $user->ideas()
+            ->selectRaw('status, count(*) as count')
+            ->groupBy('status')
+            ->pluck('count', 'status');
+
+        return collect(IdeaStatus::cases())
+            ->mapWithKeys(fn ($status) => [
+                $status->value => $counts->get($status->value, 0),
+            ])
+            ->put('all', $counts->sum());
+        // Auth::user()->ideas()->count()
+    }
 
     public function user(): BelongsTo
     {
